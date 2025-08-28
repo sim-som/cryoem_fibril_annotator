ff38ca33b744fff3cb75f4959337c4c78a26a81c
sim-som <simon.sommerhage@hhu.de>
2025-08-28 18:37:17 +0200

    todo

---
+++ convert_to_star.py
@@ -0,0 +1,11 @@
+'''
+Convert from the start end fibril coordinates to fibril segments.
+Set inter-box distance either absolut in angstrom or relative by also providing the firbril diameter.
+'''
+# %%
+import numpy as np
+import pandas as pd
+
+
+# %%
+np.load("")
